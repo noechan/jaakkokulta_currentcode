56fd0edd8f306fa7062a642213b9af357dab57ce
Noelia Martinez Molina <noelia.martinezmolina@gmail.com>
2022-03-14 11:44:09 +0100

    Update script4mydata_JKmodel.m

--- script4mydata_JKmodel.m
+++ script4mydata_JKmodel.m
@@ -1,7 +1,8 @@
 clear all
 % Define paths and init variables
-data_path='/Volumes/LASA/Aphasia_project/Acoustic_analysis/singing_stimuli/LASA Frere Jacques baseline/data/';
+data_path='/Volumes/LASA/Aphasia_project/Acoustic_analysis/singing_stimuli/FJ_Model/data/';
 code_path='/Users/noeliamartinezmolina/Documents/GitHub/jaakkokulta_currentcode/';
+output_path='/Volumes/LASA/Aphasia_project/Acoustic_analysis/singing_stimuli/FJ_Model/results/';
 addpath(code_path)
 addpath(fullfile(code_path,'yin'))
 fileExtension = '.mp3'; % replace this with e.g. 'wav' if needed
@@ -9,7 +10,7 @@ jaakkokultatemp=[0 0 2 2 4 4 0 0 0 0 2 2 4 4 0 0 4 4 5 5 7 7 7 7 4 4 5 5 7 7 7 7
 cd (data_path)
 d = dir('*_48k_1rep.mp3');
 i = 1;
-for k = 2:numel(d)
+for k = 1:numel(d)
     if contains(d(k).name,fileExtension)
         fname = d(k).name;
         out{i} = melodymatching4mydata(fname, jaakkokultatemp);
@@ -36,6 +37,9 @@ for k = 1:size(out,2)
 end
 lg = legend({'pitch curve','template'},'Orientation','Horizontal');
 lg.Layout.Tile = 'North'; % <-- Legend placement with tiled layout
+ 
+cd(output_path)
+print('Pitch curve-Template','-dpng')
 
 
 
@@ -51,8 +55,16 @@ yticklabels([pitlab rhylab])
 colorbar
 title('correlation between features')
 
+print('Correlation between features','-dpng')
+
+% Display and save feature values
 disp('Feature values')
 disp(sortrows([table(string(filename'),'VariableNames',{'filename'}),array2table(pitcherror),array2table(mean(pitcherror,2),'VariableNames',{'mean'})],'mean'))
 disp(sortrows([table(string(filename'),'VariableNames',{'filename'}),array2table(rhythmerror),array2table(mean(rhythmerror,2),'VariableNames',{'mean'})],'mean'))
 
-rmpath('yin')
+pitcherror=[table(string(filename'),'VariableNames',{'filename'}),array2table(pitcherror)];
+rhythmerror=[table(string(filename'),'VariableNames',{'filename'}),array2table(rhythmerror)];
+writetable(pitcherror, 'pitcherror_model');
+writetable(rhythmerror, 'rhythmerror_model');
+
+close()
